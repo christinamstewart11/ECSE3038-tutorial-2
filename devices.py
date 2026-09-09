3b5eefe851d809555f18895e38089ab09a6440bb
christinamstewart11 <christina.m.stewart.11@gmail.com>
2026-09-09 15:14:53 -0500

    completed task 1

---
+++ devices.py
@@ -0,0 +1,13 @@
+readings = [
+    {"name": "front-door", "room": "hall",    "temp": 27.4, "online": True},
+    {"name": "hall-lamp",  "room": "hall",    "temp": 26.1, "online": True},
+    {"name": "attic",      "room": "attic",   "temp": 31.9, "online": True},
+    {"name": "fridge",     "room": "kitchen", "temp": 4.2,  "online": False},
+    {"name": "patio",      "room": "outside", "temp": 29.8, "online": True},
+]
+
+def list_devices(readings):
+    for reading in readings:
+        print(f"The device is: {reading['name']}, The temp is: {reading['temp']}")
+        
+list_devices(readings)
